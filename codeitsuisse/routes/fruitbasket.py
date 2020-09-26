26dc783d8341880b8d0ee42113e7260d14e40687
Kai Yang <taykaiyang556@gmail.com>
2020-09-26 09:43:42 +0800

    added fruit basket

---
+++ codeitsuisse/routes/fruitbasket.py
@@ -0,0 +1,20 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+@app.route('/fruitbasket', methods=['POST'])
+def evaluate_fruitbasket():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    inputValue = data.get("input")
+    result = inputValue * inputValue
+    logging.info("My result :{}".format(result))
+    return json.dumps(result)
+
+
+
