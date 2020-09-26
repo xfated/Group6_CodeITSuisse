ba8c10cc06c55b9955cc1f9b4494a2071ae5c4a8
Kai Yang <taykaiyang556@gmail.com>
2020-09-26 09:47:57 +0800

    checking fruitbasket testcases

--- codeitsuisse/routes/fruitbasket.py
+++ codeitsuisse/routes/fruitbasket.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 def evaluate_fruitbasket():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
+    print(data)
     logging.info("My result :{}".format(result))
     return json.dumps(result)
 
