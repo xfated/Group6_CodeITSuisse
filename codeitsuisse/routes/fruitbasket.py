abfe7c30f24fc4fb96a20ca8c3d5ebada36caf55
Kai Yang <taykaiyang556@gmail.com>
2020-09-26 09:47:13 +0800

    fruitbasket

--- codeitsuisse/routes/fruitbasket.py
+++ codeitsuisse/routes/fruitbasket.py
@@ -12,7 +12,6 @@ def evaluate_fruitbasket():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     inputValue = data.get("input")
-    result = inputValue * inputValue
     logging.info("My result :{}".format(result))
     return json.dumps(result)
 
