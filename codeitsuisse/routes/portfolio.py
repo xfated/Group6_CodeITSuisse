348905d5d0e22620d90830e75dde41d2a625e346
jb <60912341+jianbin97@users.noreply.github.com>
2020-09-26 17:30:42 +0800

    Update portfolio.py

--- codeitsuisse/routes/portfolio.py
+++ codeitsuisse/routes/portfolio.py
@@ -32,25 +32,27 @@ def evaluate_portfolio():                              ## Main Function
 
         lowest = {}
         for j in index:                         #loop thru the indexes 
-            ratio =  round(j["CoRelationCoefficient"] * (port_vol / j["FuturePrcVol"]),3)
-            future_pro = round(round_ratio*value/(j["IndexFuturePrice"] * j["Notional"]))
+            ratio =  j["CoRelationCoefficient"] * (port_vol / j["FuturePrcVol"])
+            round_ratio = round(ratio,3)
+            future_pro = round_ratio*value/(j["IndexFuturePrice"] * j["Notional"])
+            future_round = round(future_pro)
             name = j['Name']
             vol = j['FuturePrcVol']                              
 
             if not any(lowest):
-                lowest['Name'], lowest['Vol'], lowest['Ratio'], lowest['Fut'] = name, vol, round_ratio, future_pro
+                lowest['Name'], lowest['Vol'], lowest['Ratio'], lowest['Fut'] = name, vol, round_ratio, future_round
             else:
                 # this future has lower vol and ratio
                 if lowest['Vol'] > vol and lowest['Ratio'] > ratio:     
-                    lowest['Name'], lowest['Vol'], lowest['Ratio'], lowest['Fut'] = name, vol, round_ratio, future_pro
+                    lowest['Name'], lowest['Vol'], lowest['Ratio'], lowest['Fut'] = name, vol, round_ratio, future_round
                 # this future has lower vol or ratio
                 elif lowest['Vol'] > vol and lowest['Ratio'] < ratio:
                     #compare lowest number of futures proportion 
                     if lowest['Fut'] > future_pro:
-                        lowest['Name'], lowest['Vol'], lowest['Ratio'], lowest['Fut'] = name, vol, round_ratio, future_pro
+                        lowest['Name'], lowest['Vol'], lowest['Ratio'], lowest['Fut'] = name, vol, round_ratio, future_round
                 elif lowest['Vol'] < vol and lowest['Ratio'] > ratio:
                     if lowest['Fut'] > future_pro:
-                        lowest['Name'], lowest['Vol'], lowest['Ratio'], lowest['Fut'] = name, vol, round_ratio, future_pro
+                        lowest['Name'], lowest['Vol'], lowest['Ratio'], lowest['Fut'] = name, vol, round_ratio, future_round
         
         best_index['HedgePositionName'] = lowest['Name']
         best_index['OptimalHedgeRatio'] = lowest['Ratio']
