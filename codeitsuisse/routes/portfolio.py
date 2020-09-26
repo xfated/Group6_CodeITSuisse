6ad5ecf6f93beb595bdbbcc8210a5ab0565285e2
jb <60912341+jianbin97@users.noreply.github.com>
2020-09-26 23:26:37 +0800

    Update portfolio.py

--- codeitsuisse/routes/portfolio.py
+++ codeitsuisse/routes/portfolio.py
@@ -14,11 +14,16 @@ def evaluate_portfolio():                              ## Main Function
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
 
-    def normal_round(n, decimals=0):
+    def normal_round(n, decimals):
         expoN = n * 10 ** decimals
         if abs(expoN) - abs(math.floor(expoN)) < 0.5:
             return math.floor(expoN) / 10 ** decimals
         return math.ceil(expoN) / 10 ** decimals
+
+    def normal(n):
+        if n - math.floor(n) < 0.5:
+            return math.floor(n)
+        return math.ceil(n)
 
 
     #get data
@@ -43,7 +48,7 @@ def evaluate_portfolio():                              ## Main Function
             ratio =  j["CoRelationCoefficient"] * (port_vol / j["FuturePrcVol"])
             round_ratio = normal_round(ratio,3)
             future_pro = round_ratio*value/(j["IndexFuturePrice"] * j["Notional"])
-            future_round = normal_round(future_pro)
+            future_round = normal(future_pro)
             name = j['Name']
             vol = j['FuturePrcVol']                              
 
@@ -55,14 +60,14 @@ def evaluate_portfolio():                              ## Main Function
                     lowest['Name'], lowest['Vol'], lowest['Ratio'], lowest['Fut'], lowest['ratio'], lowest['future'] = name, vol, round_ratio, future_round, ratio, future_pro
                 # this future has lower vol or ratio
                 elif lowest['Vol'] > vol and lowest['ratio'] < ratio :
-                    if lowest['fut'] > future_pro:
+                    if lowest['Fut'] > future_pro:
                         lowest['Name'], lowest['Vol'], lowest['Ratio'], lowest['Fut'], lowest['ratio'], lowest['future'] = name, vol, round_ratio, future_round, ratio, future_pro
                 elif lowest['Vol'] < vol and lowest['ratio'] > ratio:
-                    if lowest['fut'] > future_pro:
+                    if lowest['Fut'] > future_pro:
                         lowest['Name'], lowest['Vol'], lowest['Ratio'], lowest['Fut'], lowest['ratio'], lowest['future'] = name, vol, round_ratio, future_round, ratio, future_pro
                 # Same metrics 
                 elif lowest['Vol'] == vol and lowest['ratio'] == ratio:
-                    if lowest['fut'] > future_pro:
+                    if lowest['Fut'] > future_pro:
                         lowest['Name'], lowest['Vol'], lowest['Ratio'], lowest['Fut'], lowest['ratio'], lowest['future'] = name, vol, round_ratio, future_round, ratio, future_pro
 
 
