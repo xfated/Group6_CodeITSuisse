e8c78005af6666bf3284f2687e8ec0b6250f6b04
Kai Yang <taykaiyang556@gmail.com>
2020-09-26 14:29:03 +0800

    babylon

--- codeitsuisse/routes/olympiadbabylon.py
+++ codeitsuisse/routes/olympiadbabylon.py
@@ -19,32 +19,45 @@ def evaluate_babylon():
     books = data['books']
     days = data['days']
 
-    optim_no = 0
     all_indexes = set([i for i in range(numBooks)])
 
-    for time in days:
-        possibleCombi = {}
-        queue = [([index], books[index]) for index in all_indexes if books[index] < time]
+    day_choices = {}
+    for i in range(numDays):
+        possibleCombi = []
+        queue = [([index], books[index]) for index in all_indexes if books[index] < days[i]]
         tried_indexes = set([frozenset(indexes) for indexes, duration in queue])
         while len(queue) > 0:
             indexes, duration = queue.pop(0)
             for j in range(numBooks):
                 if j not in indexes:
                     newset = frozenset(indexes + [j])
-
                     if newset not in tried_indexes:
                         tried_indexes.add(newset)
-                        if duration + books[j] < time:
-                            queue.append((indexes + [j], duration + books[j]))
+                        if duration + books[j] < days[i]:
+                            queue.append((indexes + [j] , duration + books[j]))
                         else:
-                            possibleCombi[frozenset(indexes)] = duration
-        
-        val = max(possibleCombi.values())
-        result = [k for k,v in possibleCombi.items() if v == val]
-        day_choice = result[0]
-        for i in day_choice:
-            all_indexes.discard(i)
-        optim_no += len(day_choice)
+                            possibleCombi.append(indexes)
+        day_choices[i] = possibleCombi
+        # val = max(possibleCombi.values())
+        # result = [k for k,v in possibleCombi.items() if v == val]
+        # day_choice = result[0]
+        # for i in day_choice:
+        #     all_indexes.discard(i)
+        # optim_no += len(day_choice)
+    queue = day_choices[0]
+
+    optim_no = 0
+    for i in range(1, numDays):
+        for j in range(len(queue)):
+            prev_option = queue.pop(0)
+            for option in day_choices[i]:
+                if set(option) & set(prev_option): #there are common elements
+                    continue
+                else:
+                    sum_books = len(option) + len(prev_option)
+                    if sum_books > optim_no:
+                        optim_no = sum_books 
+                    queue.append(prev_option+option)
 
     # publish result
     logging.info("My result :{}".format(optim_no))
