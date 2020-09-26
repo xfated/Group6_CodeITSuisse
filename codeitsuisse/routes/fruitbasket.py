eda47a6e54655f66f8f2c23f84d664dea1c2c69b
Kai Yang <taykaiyang556@gmail.com>
2020-09-26 10:58:39 +0800

    ds'

--- codeitsuisse/routes/fruitbasket.py
+++ codeitsuisse/routes/fruitbasket.py
@@ -12,14 +12,13 @@ logger = logging.getLogger(__name__)
 @app.route('/fruitbasket', methods=['POST'])
 def evaluate_fruitbasket():
     data = request.get_data()
+    data = json.loads(data)
     logging.info("data sent for evaluation {}".format(data))
 
     estimate = 0
     # Parse data
-    for item in data:
-        print(item)
-    # for item in data.keys():
-    #     estimate += np.random.randint(1,100) * data[item]
+    for item in data.keys():
+        estimate += np.random.randint(1,100) * data[item]
 
     estimate = int(math.ceil(estimate/100.0))*100
     logging.info("My result :{}".format(estimate))
