90a68be9eadb3b049e95dedd1d5a85549554a485
Kai Yang <taykaiyang556@gmail.com>
2020-09-27 10:50:02 +0800

    test

--- codeitsuisse/routes/bored.py
+++ codeitsuisse/routes/bored.py
@@ -2,13 +2,18 @@ import logging
 import json
 from itertools import zip_longest
 from os import path
-import enchant
+import nltk
+nltk.download('words')
 
 from flask import request, jsonify;
 
 from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
+# setofwords = set(words.words())
+from nltk.corpus import words
+setofwords = set(words.words())
+
 
 @app.route('/bored-scribe', methods=['POST'])
 def evaluate_bored():
@@ -32,50 +37,55 @@ def evaluate_bored():
         palins = []
         # odd lengths
         for mid in range(1,len(s)-1):
-            consider = False
+            # consider = False
             temp = s[mid]
             for dist in range(1, int((len(s)+1)/2)):
                 start, end = mid-dist, mid+dist
                 if start >= 0 and end < len(s):
                     if s[start] == s[end]:
                         temp = s[start] + temp + s[end]
-                        print(temp, start, end)
-                        consider = True
-                    else:
-                        if consider == True:
-                            palins.append(temp)
-                        break
-                elif start == -1 or end == len(s):
-                    if consider == True:
                         palins.append(temp)
+                    #     consider = True
+                    else:
+                    #     if consider == True:
+                    #         palins.append(temp)
+                        break
+                # elif start == -1 or end == len(s):
+                #     if consider == True:
+                #         palins.append(temp)
         # even lengths
         for mid in range(0,len(s)-1):
-            consider = False
+            # consider = False
             temp = ''
             for dist in range(0, int((len(s)+1)/2)):
                 start, end = mid-dist, mid+1+dist
                 if start >= 0 and end < len(s):
                     if s[start] == s[end]:
                         temp = s[start] + temp + s[end]
-                        consider = True
-                    else:
-                        if consider == True:
-                            palins.append(temp)
-                        break
-                elif start == -1 or end == len(s):
-                    if consider == True:
                         palins.append(temp)
-        palins.sort()
+                #         consider = True
+                    else:
+                #         if consider == True:
+                #             palins.append(temp)
+                        break
+                # elif start == -1 or end == len(s):
+                #     if consider == True:
+                #         palins.append(temp)
+        palins = sorted(palins, key=len)
         return palins
     
     def sumString(s):
-        return sum([int(char) for char in s])
+        return sum([ord(char) for char in s])
         
-    def checkSentence(s, dic):
-        for i in range(4,7):
+    def checkSentence(s):#, dict):
+        for i in range(4,len(s)):
             for j in range(0, len(s)-i):
-                if dic.check(s[j:j+i]) == True:
-                    print('sentence', s[j:i+1])
+                # if s[j:j+i] in setofwords:
+                #     print('sentence', s[j:i+1])
+                #     return True
+                print(s[j:j+i])
+                if s[j:j+i] in setofwords:
+                    print(s[j:j+i])
                     return True
         else:
             return False
@@ -105,56 +115,81 @@ def evaluate_bored():
     #         if a is not None: yield a
     #         if b is not None: yield b
 
-    def brute(m):
-        d = enchant.Dict("en_US")
-        LETTERS = 'abcdefghijklmnopqrstuvwxyz'
-        for key in range(len(LETTERS)):
-            translated = ''
-            for symbol in m:
-                if symbol in LETTERS:
-                    num = LETTERS.find(symbol)
-                    num = num - key
+    # def brute(m):
+    #     d = enchant.Dict("en_US")
+    #     LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+    #     for key in range(len(LETTERS)):
+    #         translated = ''
+    #         for symbol in m:
+    #             if symbol in LETTERS:
+    #                 num = LETTERS.find(symbol)
+    #                 num = num - key
 
-                    if num < 0:
-                        num += len(LETTERS)
-                    translated += LETTERS[num]
+    #                 if num < 0:
+    #                     num += len(LETTERS)
+    #                 translated += LETTERS[num]
 
-                else:
-                    translated += symbol
+    #             else:
+    #                 translated += symbol
 
-            if d.check(translated):
-                return translated, key 
+    #         if d.check(translated):
+    #             return translated, key 
 
     for i in inputs:
         message = i['encryptedText']
         id_num = i['id']
         decrypt = {}
-        encrpytionCount = 0
+        
         originalText = ''
-        dict_en = enchant.Dict("en_US")
+        # dict_en = enchant.Dict("en_US")
         # longest_palin = findpalin(message)              #find longest palindrome
         # solve, key = brute(longest_palin)               #find the word and key 
         
         isValid = False
         palins = findpalin(message)
         numPalins = len(palins)
-            
-        new_message = message
-        # if numPalins = 0:
-        #     final_letter = new_message[0]
-        #     desired_shift = ord(new_message[0])
-        # while isValid == False:
-        #     if numPalins = 0:
-                
-                
-        #     else:
-        #         longestPalin = palins[-1]
-        while checkSentence(message,dict_en) == False:
-            message = shift(message, 1)
-            print(message)
-        originalText = message
 
-        encrpytionCount = 1
+        encrpytionCount = 0    
+        new_message = message
+        if numPalins == 0:
+            while isValid == False:
+                # final_letter = new_message[0]
+                for i in range(1, 26):
+                    print(i)
+                    temp = shift(new_message,i)
+                    initial_letter = temp[0]
+                    desired_shift = (ord(initial_letter) - 97 % 26)
+                    # desired_shift = abs(ord(final_letter) - ord(initial_letter))
+                    if desired_shift == 26 - i:
+                        encryptionCount += 1
+                        new_message = temp
+                        if checkSentence(new_message) == True:
+                            isValid = True
+                        break
+        else:
+            longestPalin = palins[-1]
+            while isValid == False:
+                for i in range(1,26):
+                    temp = shift(new_message,i)
+                    initLongestPalin = shift(longestPalin, i)
+                    desired_shift = (sumString(initLongestPalin) + numPalins)%26
+                    isValid = True
+                    if desired_shift == 26 - i:
+                        encrpytionCount += 1
+                        new_message = temp
+                        longestPalin = initLongestPalin    
+                        if checkSentence(new_message) == True:
+                            isValid = True 
+                        # if 'racecar' in new_message:
+                        #     print("RACECAR")
+                        break
+        originalText = new_message
+        # while checkSentence(message,dict_en) == False:
+        #     message = shift(message, 1)
+        #     print(message)
+        # originalText = message
+
+        # encrpytionCount = 1
 
         answer = {}
         answer['id'] = id_num
